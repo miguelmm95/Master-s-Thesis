cd1b40b6840b985c088382497951b8a5d5a56aa2
miguelmm95 <miguelmartinezmartinez95@gmail.com>
2025-09-19 10:37:45 +0200

    modalJavi

--- src/ReactComponents/Modals/Credits/jmmModal.js
+++ src/ReactComponents/Modals/Credits/jmmModal.js
@@ -24,7 +24,10 @@ export default function JMMModal({ visible, onClose }) {
                 <h2 className="profile-name">Javier Machin Mourelle</h2>
                 <h3 className="profile-subtitle">Digital Artist</h3>
                 <div className="about-text">
-
+                  <p>
+                    Javi is a digital artist with several years of experience. He was in charge of creating all the artwork in this portfolio.
+                    If you want to see more of his work, you can visit his <a href="https://x.com/GatlingArt" target="_blank" rel="noopener noreferrer">Twitter</a> or <a href="https://www.instagram.com/gatlingart" target="_blank" rel="noopener noreferrer">Instagram</a>.
+                  </p>
                 </div>
               </div>
             </div>
